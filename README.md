c8fb1913d0b67a5da9c1ba75cf8eee26113e2990
Mycha <m.devrees@gmail.com>
2020-10-16 16:02:36 +0200

    updated readme and added first few files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # multithreading-python
 python project to display multithreading options and race conditions
+
+## Wat is multithreading?  
+Multithreading maakt het mogelijk om verschillende taken tegelijk uit te voeren. Een proces kan gebruik maken van meerdere `threads`. Elk proces bevat een eigen geheugensegment waar meerdere threads
+toegang tot kunnen verschaffen. 
+### Wanneer gebruik je meerdere threads?  
+Als je meerdere taken hebt die tegelijkertijd uitgevoerd moeten / kunnen worden.
+### Wat zijn drie veel voorkomende problemen bij mutithreaded applications? Waardoor ontstaan ze?  
+Race conditions onstaant als beide threads dezelfde taak op hetzelfde moment uit willen voeren
+### Hoe wordt het onderdeel genoemd waar objecten in het geheugen worden geplaatst? Hoe is dit verschillend in een multithreaded application?
+The Stack is tijdens het uitvoeren van een code block verantwoordelijk voor het opslaan van pointers naar objecten in het geheugen
+en het afdwingen van zichtbaarheid van variabelen (scope, public/private). Elke thread heeft zijn eigen stack.
+### Hoe wordt het onderdeel genoemd waar methoden worden uitgevoerd en primitive types in het geheugen worden geplaatst? Hoe is dit verschillend in een multithreaded application?
+The Heap, hier worden de objecten opgeslagen. The Stack heeft alleen een pointer / verwijzing naar de objecten in The Heap. Alle threads
+delen dezelfde heap
+### Wat is in dit kader een racing condition? Hoe zou je dit kunnen voorkomen? 
+Een race condition kan voorkomen worden op meerdere manieren, een hiervan is bijvoorbeeld een lock. Een lock is een soort gangpas,
+een enkele thread kan de lock hebben en als een andere thread de lock wilt hebben, moet deze wachten totdat deze vrijgegeven is.
+
+#### Bronnen
+https://techacademy.id.nl/blog/wat-is-multithreading-en-wat-heb-je-er-aan/
+https://stackoverflow.com/questions/1665419/do-threads-have-a-distinct-heap
+https://realpython.com/intro-to-python-threading/
---
+++ main.py
@@ -0,0 +1,65 @@
+import logging
+import threading
+import time
+from race_condition import FakeDatabase
+import concurrent.futures
+
+
+def thread_function(name):
+    logging.info("Thread %s: starting", name)
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+
+def single_thread():
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    logging.info("Main    : before creating thread")
+    x = threading.Thread(target=thread_function, args=(1,))
+    logging.info("Main    : before running thread")
+    x.start()
+    logging.info("Main    : wait for the thread to finish")
+    # x.join()
+    logging.info("Main    : all done")
+
+
+def multi_thread():
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    threads = list()
+    for index in range(3):
+        logging.info("Main    : create and start thread %d.", index)
+        x = threading.Thread(target=thread_function, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main    : before joining thread %d.", index)
+        thread.join()
+        logging.info("Main    : thread %d done", index)
+
+
+def race_condition():
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+
+    database = FakeDatabase()
+    logging.info("Testing update. Starting value is %d.", database.value)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        for index in range(2):
+            executor.submit(database.update, index)
+    logging.info("Testing update. Ending value is %d.", database.value)
+
+
+def main():
+    # single_thread()
+    # multi_thread()
+    race_condition()
+
+
+if __name__ == "__main__":
+    main()
